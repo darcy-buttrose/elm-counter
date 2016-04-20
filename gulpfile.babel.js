a42d57aa4cec1f35b728a8be326e285216851915
darcy-buttrose <darcy.buttrose@gmail.com>
2016-04-20 21:53:53 +0800

    first working copy

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,32 +1,18 @@
 import gulp from 'gulp';
-import typescript from 'gulp-typescript';
-import babel from 'gulp-babel';
-import mocha from 'gulp-mocha';
 import elm from 'gulp-elm';
-import webpack from 'webpack-stream';
-import 'babel-polyfill';
 
 gulp.task('elm-init', elm.init);
 
-gulp.task('compile', ['elm-init'], () => {
-    return gulp.src('./src/**/*.elm*')
-        .pipe(elm())
-        .pipe(gulp.dest('./build'));
+gulp.task('elm', ['elm-init'], () => {
+  return gulp.src('src/*.elm')
+    .pipe(elm())
+    .pipe(gulp.dest('dist/'));
 });
 
-gulp.task('package', ['compile'],function() {
-    return gulp.src('./build/code/main.js')
-        .pipe(webpack({
-            devtool: 'sourse-map',
-            output: {
-                filename: 'bundle.js'
-            }
-        }))
-        .pipe(gulp.dest('./dist'));
+gulp.task('elm-bundle', ['elm-init'], () => {
+  return gulp.src('src/*.elm')
+    .pipe(elm.bundle('bundle.js'))
+    .pipe(gulp.dest('dist/'));
 });
 
-gulp.task('watch',['package'],function(){
-    gulp.watch(['./src/**/*.ts'],['package']);
-})
-
-gulp.task('default',['package']);
+gulp.task('default',['elm-bundle']);
--- src/main.elm
+++ src/main.elm
@@ -1,23 +1,5 @@
-module Counter where
+import Counter exposing (update,view)
+import StartApp.Simple exposing (start)
 
-import Html exposing (..)
-import Html.Attributes exposing (style)
-import Html.Events exposing (onClick)
-
-type alias Model = Int
-
-type Action = Increment | Decrement
-
-update : Action -> Model -> Model
-update action model =
-    case action of
-        Increment -> Model + 1
-        Decrement -> Model - 1
-        
-view : Signal.Address Action -> Model -> Html
-view address model =
-    div []
-        [ button [ onClick address Decrement ] [ text "-" ]
-        , div [  ]
-        ]
-        
+main =
+    start { model = 0, update = update, view = view }
